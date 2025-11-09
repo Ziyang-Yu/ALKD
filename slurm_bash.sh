c59a3e102f712a5141acc3e130d7ca7b55063c06
Ziyang Yu <ziyang.yu@emory.edu>
2025-11-08 21:56:43 -0500

    init

---
+++ slurm_bash.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --job-name=badge
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu:1
+#SBATCH --mem=48G
+#SBATCH --time=12:00:00
+#SBATCH --output=logs/badge_%j.out
+#SBATCH --error=logs/badge_%j.err
+#SBATCH --partition=h200
+
+
+set -euo pipefail
+
+source /local/scratch/zyu273/miniconda3/etc/profile.d/conda.sh
+# conda create -p /local/scratch/zyu273/alkd/env python=3.10 -y
+conda activate /local/scratch/zyu273/alkd/env
+# pip install torch==2.6.0 torchvision==0.21.0 torchaudio==2.6.0 --index-url https://download.pytorch.org/whl/cu124
+# pip install -U scikit-learn
+# pip install numpy pandas tqdm
+# pip install openml
+# python run.py --alg badge --did 0 --lr 0.001  --model resnet --data data --nQuery 10000 --nStart 10000 --nEnd 50000 --nEmb 256 --trunc -1 --aug 1 --dummy 1 --data CIFAR10 --lr 0.001 
+
+# python run.py --alg badge --did 0 --lr 0.001  --model resnet --data data --nQuery 10000 --nStart 10000 --nEnd 50000 --nEmb 256 --trunc -1 --aug 1 --dummy 1 --data CIFAR10 --lr 0.001 
+# python run.py --model mlp --nQuery 10000 --did 6 --alg bait
+python badge_agnews.py \
+  --epochs 2 --rounds 5 \
+  --batch_size 96 --eval_batch_size 256 \
+  --query_size 1000 --seed_size 200 \
+  --max_length 160 --max_pool 60000
