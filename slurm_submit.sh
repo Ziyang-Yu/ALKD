85d64dbc1c8cbd0c954bc4464560e5d001d2d1d8
Ziyang Yu <z333yu@uwaterloo.ca>
2025-11-14 21:20:45 -0500

    Auto commit: 2025-11-14 21:20:45 - updated code & results

--- slurm_submit.sh
+++ slurm_submit.sh
@@ -1,14 +1,14 @@
-sbatch scripts/original/run_badge_agnews.sh
-sbatch scripts/original/run_badge_amazon.sh
-sbatch scripts/original/run_badge_imdb.sh
-sbatch scripts/original/run_badge_yelp.sh
+# sbatch scripts/original/run_badge_agnews.sh
+# sbatch scripts/original/run_badge_amazon.sh
+# sbatch scripts/original/run_badge_imdb.sh
+# sbatch scripts/original/run_badge_yelp.sh
 
-# sbatch scripts/llm/run_badge_agnews_llm.sh
-# sbatch scripts/llm/run_badge_amazon_llm.sh
-# sbatch scripts/llm/run_badge_imdb_llm.sh
-# sbatch scripts/llm/run_badge_yelp_llm.sh
+sbatch scripts/llm/run_badge_agnews_llm.sh
+sbatch scripts/llm/run_badge_amazon_llm.sh
+sbatch scripts/llm/run_badge_imdb_llm.sh
+sbatch scripts/llm/run_badge_yelp_llm.sh
 
-# sbatch scripts/llm_cbm/run_badge_agnews_llm_cbm.sh
-# sbatch scripts/llm_cbm/run_amazon_llm_cbm.sh
-# sbatch scripts/llm_cbm/run_imdb_llm_cbm.sh
-# sbatch scripts/llm_cbm/run_yelp_llm_cbm.sh
+sbatch scripts/llm_cbm/run_badge_agnews_llm_cbm.sh
+sbatch scripts/llm_cbm/run_amazon_llm_cbm.sh
+sbatch scripts/llm_cbm/run_imdb_llm_cbm.sh
+sbatch scripts/llm_cbm/run_yelp_llm_cbm.sh
