9cccab6548ec8b882ae5eb7ac4132a8752204d9b
Ziyang Yu <z333yu@uwaterloo.ca>
2025-11-15 20:23:30 -0500

    Auto commit: 2025-11-15 20:23:30 - updated code & results

--- slurm_submit.sh
+++ slurm_submit.sh
@@ -4,11 +4,11 @@
 # sbatch scripts/original/run_badge_yelp.sh
 
 # sbatch scripts/llm/run_badge_agnews_llm.sh
-# sbatch scripts/llm/run_badge_amazon_llm.sh
+sbatch scripts/llm/run_badge_amazon_llm.sh
 # sbatch scripts/llm/run_badge_imdb_llm.sh
-sbatch scripts/llm/run_badge_yelp_llm.sh
+# sbatch scripts/llm/run_badge_yelp_llm.sh
 
 # sbatch scripts/llm_cbm/run_badge_agnews_llm_cbm.sh
-# sbatch scripts/llm_cbm/run_badge_amazon_llm_cbm.sh
+sbatch scripts/llm_cbm/run_badge_amazon_llm_cbm.sh
 # sbatch scripts/llm_cbm/run_badge_imdb_llm_cbm.sh
-sbatch scripts/llm_cbm/run_badge_yelp_llm_cbm.sh
+# sbatch scripts/llm_cbm/run_badge_yelp_llm_cbm.sh
